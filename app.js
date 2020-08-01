fb674017052f67ad970afcf1fdedf20c3e3b735d
Štěpán Zoubek <stepan.zoubek@gmail.com>
2020-08-02 00:10:58 +0200

    Post new article

--- app.js
+++ app.js
@@ -40,16 +40,30 @@ const Article = mongoose.model('Article', articlesSchema)
 
 //app
 
-app.get('/articles', (req,res)=>{
-    Article.find({},(err, articles)=>{
-        if(!err){
-            res.send(articles)  
-        }else{
-            res.send(err)
-        }
-    })
+app.get('/articles', (req, res) => {
+  Article.find({}, (err, articles) => {
+    if (!err) {
+      res.send(articles)
+    } else {
+      res.send(err)
+    }
+  })
 })
 
+app.post('/articles', (req, res) => {
+  console.log(req.body.title, req.body.content)
+  const article = new Article({
+    title: req.body.title,
+    content: req.body.content
+  })
+  article.save(err => {
+    if (err) {
+      res.send(err)
+    } else {
+      res.send('Article added')
+    }
+  })
+})
 
 app.listen(port, () => {
   console.log(`App is running on port:${port}`)
